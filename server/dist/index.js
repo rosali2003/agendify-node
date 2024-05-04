77e44c223bee0cf8431b84cab91e3ebef46083b9
Rosa Li <rosali.tks@gmail.com>
2024-05-04 15:31:17 -0400

    created post request to add tasks

--- server/dist/index.js
+++ server/dist/index.js
@@ -6,12 +6,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
+const helpers_1 = require("./helper/helpers");
 const Task_js_1 = require("./models/Task.js");
 const app = (0, express_1.default)();
 const PORT = 5000;
 dotenv_1.default.config();
 // Middleware for parsing request body
-app.use(express_1.default.json());
+app.use(express_1.default.json(), (0, cors_1.default)({
+    origin: [process.env.CLIENT_URL],
+    credentials: true,
+}));
 app.listen(PORT, () => {
     console.log(`App is listening to port: ${PORT}`);
 });
@@ -19,21 +24,27 @@ app.get("/", (request, response) => {
     console.log(request);
     return response.status(234).send("response sent");
 });
-const newTask = {
-    id: 1,
-    message: 'clean room',
-    completed: false,
-};
-app.post("/tasks", async (request, response) => {
+// const newTask = {
+//   id: 1,
+//   message: "clean room",
+//   completed: false,
+// };
+app.post("/add_task", async (request, response) => {
+    const data = request.body.data;
+    console.log('data', data);
     try {
-        if (!request.body.id || !request.body.message || !request.body.completed) {
+        if (!(0, helpers_1.allPresent)(data)) {
+            console.log((!data.id || !data.message || !data.completed));
+            console.log('data.id present', (0, helpers_1.present)(data.id));
+            console.log('data.message present', (0, helpers_1.present)(data.message));
+            console.log('data.completed', (0, helpers_1.present)(data.completed));
             return response.status(400).send({
                 message: "Send all required fields: message, completed",
             });
         }
-        const task = await Task_js_1.Task.create(newTask);
+        const task = await Task_js_1.Task.create(data);
         console.log("task successfully created");
-        return response.status(201).send(newTask);
+        return response.status(201).send(data);
     }
     catch (error) {
         console.log(error.message);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,21 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv';
-import {Task} from './models/Task.js';
-// import {oauth} from './oauth.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import cors from "cors";
+import {allPresent, present} from './helper/helpers';
+import { Task } from "./models/Task.js";
 const app = express();
 const PORT = 5000;
 dotenv.config();
 
 // Middleware for parsing request body
-app.use(express.json());
+app.use(
+  express.json(),
+  cors({
+    origin: [process.env.CLIENT_URL],
+    credentials: true,
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`App is listening to port: ${PORT}`);
@@ -19,30 +25,39 @@ app.get("/", (request, response) => {
   return response.status(234).send("response sent");
 });
 
-
-const newTask = {
-  id: 1,
-  message: 'clean room',
-  completed: false,
-}
-
-
+//create new task
 app.post("/add_task", async (request, response) => {
+  const data = request.body.data;
+  console.log('data',data)
   try {
-    if (!request.body.id || !request.body.message || !request.body.completed) {
+    if (!allPresent(data)) {
+      console.log((!data.id || !data.message || !data.completed));
+      console.log('data.id present', present(data.id))
+      console.log('data.message present',present(data.message))
+      console.log('data.completed', present(data.completed))
       return response.status(400).send({
         message: "Send all required fields: message, completed",
       });
     }
-    const task = await Task.create(newTask);
+    const task = await Task.create(data);
     console.log("task successfully created");
-    return response.status(201).send(newTask);
+    return response.status(201).send(data);
   } catch (error) {
     console.log(error.message);
     response.status(500).send({ message: error.message });
   }
 });
 
+//update task completed
+// app.patch('/edit_completed', async (request, response) => {
+//   const data = request.body.data;
+//   console.log(data);
+//   try {
+//     if 
+//   }
+// })
+
+app.patch('/edit')
 mongoose
   .connect(process.env.URI)
   .then(() => {
